7ba395b51c695031c364b00c7b74276e8743e610
Lucas <lucaslcsteam@hotmail.com>
2022-11-14 11:19:35 -0300

    menu feito

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  games = [
+    {
+      title: 'Jogo da Velha',
+      description: 'Dois jogadores se enfretam para ver quem consegue completar a linha com seu X ou O.',
+      url: '/tictactoe',
+      image: '../../../assets/image/blur.png'
+    },
+    {
+      title: 'ToDo List',
+      description: 'Uma lista de afazeres, sendo possível adicionar, editar e excluir atividades.',
+      url: '/todo',
+      image: '../../../assets/image/blur.png'
+    },
+    {
+      title: '...?',
+      description: 'Em breve.',
+      url: '/',
+      image: '../../../assets/image/blur.png'
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
